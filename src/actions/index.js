339517e7cd9db306a23906f7b1f0f4502e9989fe
Yumeng <wym7230@gmail.com>
2017-10-11 11:39:32 -0700

    fixed pi & sign cannot be calculated by sin

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,27 @@
+import _ from 'lodash';
+
 export const APPEND_VALUE = "APPEND_VALUE";
 export const EQUAL_OPERATION = "EQUAL_OPERATION";
 export const CLEAR_OPERATION = "CLEAR_OPEATION";
+
+const binaryOperand = new Set(["/", "+", "-", "*"]);
+
+const operandMap = {
+  π: Math.PI,
+  e: Math.E
+};
 
 export const onButtonPressed = symbol => {
   return async (dispatch, getState) => {
     // current is a string expression
     const { current, isTyping } = getState().display;
-    const currentNumber = parseFloat(current);
+    let currentNumber;
+    if (!_.isUndefined(operandMap[current])) {
+      currentNumber = operandMap[current];
+    }else {
+      currentNumber = parseFloat(current);
+    }
+
     let payload;
     switch (symbol) {
       case "C":
@@ -15,35 +30,26 @@ export const onButtonPressed = symbol => {
         });
         break;
       case "±":
-        if (!isNaN(currentNumber)) {
-          payload =
-            `${currentNumber < 0 ? Math.abs(currentNumber) : -currentNumber}`;
-          dispatchResultToReducer(dispatch, APPEND_VALUE, payload);
-        }
+        payload = `${currentNumber < 0
+          ? Math.abs(currentNumber)
+          : -currentNumber}`;
+        dispatchResultToReducer(dispatch, APPEND_VALUE, payload);
         break;
       case "%":
-        if (!isNaN(currentNumber)) {
-          payload = `${(currentNumber / 100).toFixed(2)}`;
-          dispatchResultToReducer(dispatch, EQUAL_OPERATION, payload);
-        }
+        payload = `${(currentNumber / 100).toFixed(2)}`;
+        dispatchResultToReducer(dispatch, EQUAL_OPERATION, payload);
         break;
       case "sin":
-        if (!isNaN(currentNumber)) {
-          payload = `${Math.sin(currentNumber)}`;
-          dispatchResultToReducer(dispatch, EQUAL_OPERATION, payload);
-        }
+        payload = `${Math.sin(currentNumber)}`;
+        dispatchResultToReducer(dispatch, EQUAL_OPERATION, payload);
         break;
       case "√":
-        if (!isNaN(currentNumber)) {
-          payload = `${Math.sin(currentNumber)}`;
-          dispatchResultToReducer(dispatch, EQUAL_OPERATION, payload);
-        }
+        payload = `${Math.sin(currentNumber)}`;
+        dispatchResultToReducer(dispatch, EQUAL_OPERATION, payload);
         break;
       case "²":
-        if (!isNaN(currentNumber)) {
-          payload = `${currentNumber * currentNumber}`
-          dispatchResultToReducer(dispatch, EQUAL_OPERATION, payload);
-        }
+        payload = `${currentNumber * currentNumber}`;
+        dispatchResultToReducer(dispatch, EQUAL_OPERATION, payload);
         break;
       case "/":
       case "+":
@@ -59,22 +65,18 @@ export const onButtonPressed = symbol => {
         }
         break;
       case "=":
-        const operandMap = {
-          'π': Math.PI,
-          'e': Math.E,
-        }
         const expression = current.replace(/π|e/g, function(matched) {
           return operandMap[matched];
         });
         try {
           payload = `${eval(expression)}`;
           dispatchResultToReducer(dispatch, EQUAL_OPERATION, payload);
-        } catch(e) {
-          dispatchResultToReducer(dispatch, EQUAL_OPERATION, 'NaN');
+        } catch (e) {
+          dispatchResultToReducer(dispatch, EQUAL_OPERATION, "NaN");
         }
         break;
       default:
-        if ((current === "0" || !isTyping) && symbol !== '.') {
+        if ((current === "0" || !isTyping) && symbol !== ".") {
           dispatchResultToReducer(dispatch, APPEND_VALUE, symbol);
         } else {
           dispatchResultToReducer(dispatch, APPEND_VALUE, current + symbol);
@@ -82,8 +84,6 @@ export const onButtonPressed = symbol => {
     }
   };
 };
-
-const binaryOperand = new Set(["/", "+", "-", "*"]);
 
 const dispatchResultToReducer = (dispatch, type, payload) => {
   return dispatch({ type, payload });
